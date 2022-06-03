90bfb7fa174cf59427c36a1aec2ddffd8b4865e3
Xettri Aleen <54938235+xettrialeen@users.noreply.github.com>
2022-06-03 21:27:46 +0545

    gg

--- src/js/script.js
+++ src/js/script.js
@@ -635,11 +635,11 @@ let hamMenu = gsap.timeline({
 gsap.to(
   ".ball",
   {
-    width: 5,
-    height: 5,
+    width: 6,
+    height: 6,
     borderRadius: "50%",
     duration: 0.3,
-    marginBottom: 5,
+    marginBottom: 4,
     x: 10,
 
     ease: "easeInOut",
@@ -649,15 +649,15 @@ gsap.to(
 hamMenu.fromTo(
   ".ball",
   {
-    y: 6,
-
-    duration: 0.86,
+    y: 4.5,
+  
+    duration: 0.88,
     ease: "linear",
   },
   {
-    y: -6,
+    y: -4.5,
 
-    duration: 0.86,
+    duration: 0.88,
     ease: "linear",
   },
   "0"
@@ -669,15 +669,15 @@ let batTwoTimeline = batTimeline;
 batTimeline.fromTo(
   ".bat__1",
   {
-    x: 4,
+    x: 3,
     ease: "linear",
- 
+   
     opacity:0
   },
   {
-    x: -4,
+    x: -3,
     ease: "linear",
-    duration: 0.88,
+    duration: 0.91,
     opacity:1
   },
   "0"
@@ -685,15 +685,15 @@ batTimeline.fromTo(
 batTwoTimeline.fromTo(
   ".bat__2",
   {
-    x: -4,
+    x: -3,
     ease: "linear",
-    duration: 0.88,
+
     opacity:1
   },
   {
-    x: 4,
+    x: 3,
     ease: "linear",
-   
+    duration: 0.91,
     opacity:0
   },
   "0"
@@ -843,3 +843,88 @@ batTwoTimeline.fromTo(
 //   )
 
 //   ;
+
+
+window.onload = () => {
+  // init the notification
+  let notification = document.querySelectorAll(".notification");
+  console.log(notification);
+  notification.forEach((event) => {
+    event.addEventListener("click", () => {
+      let notSound = document.getElementsByClassName("NotificationSound");
+
+      notSound[0].play();
+    });
+  });
+};
+
+// movieng Cursor
+
+const ball = document.querySelector(".circle");
+
+let mouseX = 1480;
+let mouseY = 120;
+
+let ballX = 0;
+let ballY = 0;
+
+let speed = 0.06;
+
+function animate() {
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+
+  ballX = ballX + distX * speed;
+  ballY = ballY + distY * speed;
+
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+let menu = document.querySelector(".menu");
+menu.addEventListener("mousemove", function (event) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+});
+// menu.addEventListener("mouseleave", function (event) {
+//   ball.style = `visibility: hidden; opacity:0; transition:all 0.3 ease-in-out;`;
+
+// });
+menu.addEventListener("mouseenter", function (event) {
+  ball.style = `visibility: visible; opacity:1;  transition:all 0.3 ease-in-out;`;
+});
+menu.addEventListener("mouseleave", function (event) {
+  ball.style = `visibility: hidden; opacity:0;  transition:all 0.3 ease-in-out;`;
+});
+
+let navLink = document.querySelectorAll(".cursor__hover ");
+
+for (const i of navLink) {
+  i.addEventListener("mouseenter", () => {
+    ball.classList.add("circleActive");
+    ball.classList.remove("circleRemove");
+  });
+  i.addEventListener("mouseleave", () => {
+    ball.classList.add("circleRemove");
+    ball.classList.remove("circleActive");
+  });
+}
+// navLink.foreach((e) => {
+//   e.addEventListener("mouseenter", () => {
+//     console.log(e);
+//     ball.classList.add("circleActive");
+//   });
+// });
+
+let menuBtn = document.getElementsByClassName(".menuBtn");
+let btnState= true;
+menuBtn[0].addEventListener("click",()=>{
+  btnState= !btnState;
+ if (btnState === true) {
+  ball.style = `display: none;`;
+ }
+ 
+})
--- src/js/sound.js
+++ src/js/sound.js
@@ -1,83 +1,83 @@
-window.onload = () => {
-  // init the notification
-  let notification = document.querySelectorAll(".notification");
-  console.log(notification);
-  notification.forEach((event) => {
-    event.addEventListener("click", () => {
-      let notSound = document.getElementsByClassName("NotificationSound");
+// window.onload = () => {
+//   // init the notification
+//   let notification = document.querySelectorAll(".notification");
+//   console.log(notification);
+//   notification.forEach((event) => {
+//     event.addEventListener("click", () => {
+//       let notSound = document.getElementsByClassName("NotificationSound");
 
-      notSound[0].play();
-    });
-  });
-};
-
-// movieng Cursor
-
-const ball = document.querySelector(".circle");
-
-let mouseX = 1480;
-let mouseY = 120;
-
-let ballX = 0;
-let ballY = 0;
-
-let speed = 0.06;
-
-function animate() {
-  let distX = mouseX - ballX;
-  let distY = mouseY - ballY;
-
-  ballX = ballX + distX * speed;
-  ballY = ballY + distY * speed;
-
-  ball.style.left = ballX + "px";
-  ball.style.top = ballY + "px";
-
-  requestAnimationFrame(animate);
-}
-animate();
-
-let menu = document.querySelector(".menu");
-menu.addEventListener("mousemove", function (event) {
-  mouseX = event.pageX;
-  mouseY = event.pageY;
-});
-// menu.addEventListener("mouseleave", function (event) {
-//   ball.style = `visibility: hidden; opacity:0; transition:all 0.3 ease-in-out;`;
-
-// });
-menu.addEventListener("mouseenter", function (event) {
-  ball.style = `visibility: visible; opacity:1;  transition:all 0.3 ease-in-out;`;
-});
-menu.addEventListener("mouseleave", function (event) {
-  ball.style = `visibility: hidden; opacity:0;  transition:all 0.3 ease-in-out;`;
-});
-
-let navLink = document.querySelectorAll(".cursor__hover ");
-
-for (const i of navLink) {
-  i.addEventListener("mouseenter", () => {
-    ball.classList.add("circleActive");
-    ball.classList.remove("circleRemove");
-  });
-  i.addEventListener("mouseleave", () => {
-    ball.classList.add("circleRemove");
-    ball.classList.remove("circleActive");
-  });
-}
-// navLink.foreach((e) => {
-//   e.addEventListener("mouseenter", () => {
-//     console.log(e);
-//     ball.classList.add("circleActive");
+//       notSound[0].play();
+//     });
 //   });
+// };
+
+// // movieng Cursor
+
+// const ball = document.querySelector(".circle");
+
+// let mouseX = 1480;
+// let mouseY = 120;
+
+// let ballX = 0;
+// let ballY = 0;
+
+// let speed = 0.06;
+
+// function animate() {
+//   let distX = mouseX - ballX;
+//   let distY = mouseY - ballY;
+
+//   ballX = ballX + distX * speed;
+//   ballY = ballY + distY * speed;
+
+//   ball.style.left = ballX + "px";
+//   ball.style.top = ballY + "px";
+
+//   requestAnimationFrame(animate);
+// }
+// animate();
+
+// let menu = document.querySelector(".menu");
+// menu.addEventListener("mousemove", function (event) {
+//   mouseX = event.pageX;
+//   mouseY = event.pageY;
+// });
+// // menu.addEventListener("mouseleave", function (event) {
+// //   ball.style = `visibility: hidden; opacity:0; transition:all 0.3 ease-in-out;`;
+
+// // });
+// menu.addEventListener("mouseenter", function (event) {
+//   ball.style = `visibility: visible; opacity:1;  transition:all 0.3 ease-in-out;`;
+// });
+// menu.addEventListener("mouseleave", function (event) {
+//   ball.style = `visibility: hidden; opacity:0;  transition:all 0.3 ease-in-out;`;
 // });
 
-let menuBtn = document.getElementsByClassName(".menuBtn");
-let btnState= true;
-menuBtn[0].addEventListener("click",()=>{
-  btnState= !btnState;
- if (btnState === true) {
-  ball.style = `display: none;`;
- }
+// let navLink = document.querySelectorAll(".cursor__hover ");
+
+// for (const i of navLink) {
+//   i.addEventListener("mouseenter", () => {
+//     ball.classList.add("circleActive");
+//     ball.classList.remove("circleRemove");
+//   });
+//   i.addEventListener("mouseleave", () => {
+//     ball.classList.add("circleRemove");
+//     ball.classList.remove("circleActive");
+//   });
+// }
+// // navLink.foreach((e) => {
+// //   e.addEventListener("mouseenter", () => {
+// //     console.log(e);
+// //     ball.classList.add("circleActive");
+// //   });
+// // });
+
+// let menuBtn = document.getElementsByClassName(".menuBtn");
+// let btnState= true;
+// menuBtn[0].addEventListener("click",()=>{
+//   btnState= !btnState;
+//  if (btnState === true) {
+//   ball.style = `display: none;`;
+//  }
  
-})
+// })
